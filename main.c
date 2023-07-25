7dd35e37662773cca3eabb01a79f0d8235c31f5e
i475yama <138637492+i475yama@users.noreply.github.com>
2023-07-25 13:24:44 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,28 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
+double myRoot(double x) // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。 
 {
   int y;
-  if( x <= 0 ){
+  if( !(x >= 0) ){
     exit(1);
   }
   y = sqrt(x);
   return y;
+}
+
+void foo(double x)
+{
+  if( x > 0 ){
+    printf("正の値です。\n");
+  }else if( x == 0 ){
+    printf("0です。\n");
+  }else if( x < 0 ){
+    printf("負の値です。\n");
+  }else{
+    printf("数ではないです。\n");
+  }
 }
 
 int main(void)
